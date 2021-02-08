baf3b1c104ccee0a24aaf956fca6a94cf7099c4b
Mike Wilder <mikewilder01@gmail.com>
2021-02-08 18:50:18 -0500

    Initial commit

---
+++ db/migrate/20210208210501_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.date :dob
+
+      t.timestamps null: false
+    end
+  end
+end
