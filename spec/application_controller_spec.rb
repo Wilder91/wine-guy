f74c5071f9eb1d7309da93d16e5894b2218ccda7
Mike Wilder <mikewilder01@gmail.com>
2021-03-02 23:38:46 -0500

    one last commit before review

--- spec/application_controller_spec.rb
+++ spec/application_controller_spec.rb
@@ -5,7 +5,7 @@ def app
 end
 
 describe ApplicationController do
-  it "responds with a welcome message" do
+  it "Responds with a welcome message" do
     get '/'
     expect(last_response.status).to eq(200)
     expect(last_response.body).to include("Welcome to the Sinatra Template!")
